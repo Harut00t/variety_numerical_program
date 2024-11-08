3c9411a01077e20b483847341d201ca82e7e0611
@Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-11-08 12:37:28 +0800

    Last changes, easily donegit add variety_numerical_program.py !

--- variety_numerical_program.py
+++ variety_numerical_program.py
@@ -10,12 +10,11 @@ print("8 - division number")
 
 user_input = int(input("where do you want to proceed?: "))
 
-num1 = int(input("insert a 1st number here: "))
-num2 = int(input("insert a 2nd number here: "))
-
 # Create a program that ask user to input 2 numbers. Print the bigger number.
 
 def find_big_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
     
     if num1 > num2:
         print(f"the biggest number is {num1}")
@@ -25,6 +24,8 @@ def find_big_num(num1, num2):
 # Create a program that ask user to input 2 numbers. Print the lower number.
 
 def find_low_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
     
     if num1 > num2:
         print(f"the lowest number is {num2}")
@@ -34,6 +35,8 @@ def find_low_num(num1, num2):
 # Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
 def find_equal_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
 
     if num1 == num2:
         print("Equal")
@@ -43,6 +46,8 @@ def find_equal_num(num1, num2):
 # Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 def find_not_equal_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
 
     if num1 != num2:
         print("Not Equal")
@@ -52,73 +57,98 @@ def find_not_equal_num(num1, num2):
 # Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 def find_sum_of_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
 
     sum = num1 + num2
-    print(sum)
+    print(f"the answer is {sum}")
     
 # Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
 def find_diff_of_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
 
     diff = num1 - num2
-    print(diff)
+    print(f"the answer is {diff}")
 
 # Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 def find_prod_of_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
 
     prod = num1 * num2
-    print(prod)
+    print(f"the answer is {prod}")
 
 # Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
 def find_quot_of_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
 
     quot = num1 // num2
-    print(quot)
+    print(f"the answer is {quot}")
     
 # Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
-# Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
-# Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
-# Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
-# Create a program that ask user to input 10 numbers. Print the difference of all the numbers.
-# Create a program that ask user to input 10 numbers. Print how many are odd numbers.
-# Create a program that ask user to input 10 numbers. Print how many are even numbers.
-# Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
-# Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
-# Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
-# Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
-# Create a program that ask user to input 2 numbers. When the first is greater than the second. Print all the numbers between the two.
 
+def find_float_quot_of_num(numflo1, numflo2):
+    numflo1 = float(input("Insert a 1st number here: "))
+    numflo2 = float(input("Insert a 2nd number here: "))
+
+    float_quot = numflo1 / numflo2
+    print(f"the answer is {float_quot}")
+
+# Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+def find_remain_quot_of_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
+
+    remain_quot = (num1 % num2)
+    print(f"the answer is {remain_quot}")
+
+# Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+def find_raised_of_num(num1, num2):
+    num1 = int(input("insert a 1st number here: "))
+    num2 = int(input("insert a 2nd number here: "))
+
+    raised_num = (num1 ** num2)
+    print(f"the answer is {raised_num}")
 
 if user_input == 1:
-    result_big = find_big_num(num1, num2)
-    print(f"{result_big}")
+    find_big_num()
 
-if user_input == 2:
-    result_low = find_low_num(num1, num2)
-    print(f"{result_low}")
+elif user_input == 2:
+    find_low_num()
 
-if user_input == 3:
-    result_equal = find_equal_num(num1, num2)
-    print(f"{result_equal}")
+elif user_input == 3:
+    find_equal_num()
 
-if user_input == 4:
-    result_not_equal = find_not_equal_num(num1, num2)
-    print(f"{result_not_equal}")
+elif user_input == 4:
+    find_not_equal_num()
 
-if user_input == 5:
-    result_sum = find_sum_of_num(num1, num2)
-    print(f"{result_sum}")
+elif user_input == 5:
+    find_sum_of_num()
 
-if user_input == 6:
-    result_diff = find_diff_of_num(num1, num2)
-    print(f"{result_diff}")
+elif user_input == 6:
+    find_diff_of_num()
 
-if user_input == 7:
-    result_prod = find_prod_of_num(num1, num2)
-    print(f"{result_prod}")
+elif user_input == 7:
+    find_prod_of_num()
 
-if user_input == 8:
-    result_quot = find_quot_of_num(num1, num2)
-    print(f"{result_quot}")
+elif user_input == 8:
+    find_quot_of_num()
+
+elif user_input == 9:
+    find_float_quot_of_num()
+
+elif user_input == 11:
+    find_remain_quot_of_num()
+
+elif user_input == 12:
+    find_raised_of_num()
+
+else:
+    print("please try again.")
