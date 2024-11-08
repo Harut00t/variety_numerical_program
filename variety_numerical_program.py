417e8c818908285f6b3c335f4e6f0bf7d321e49d
@Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-11-08 10:55:03 +0800

    This is a program that will try to be a program with many functions.

---
+++ variety_numerical_program.py
@@ -0,0 +1,20 @@
+# Create a program that ask user to input 2 numbers. Print the bigger number.
+# Create a program that ask user to input 2 numbers. Print the lower number.
+# Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+# Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+# Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+# Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+# Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+# Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+# Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+# Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+# Create a program that ask user to input 10 numbers. Print the difference of all the numbers.
+# Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+# Create a program that ask user to input 10 numbers. Print how many are even numbers.
+# Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+# Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+# Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+# Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+# Create a program that ask user to input 2 numbers. When the first is greater than the second. Print all the numbers between the two.
